fe7edb4bd3bffb1babe8762acc735851201fea51
TeoStorm7 <teodorfurtuna@yahoo.com>
2023-09-10 22:57:18 +0300

    Add routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,19 @@
-import logo from "./logo.svg";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import Home from "./views/Home";
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h1>Boscanici Adrian Gabriel</h1>
-        <h1>Bogdan Dulgheriu gabriel</h1>
-        <h1>Stefan Ionel</h1>
-        <h1>Miloiu Cristi</h1>
-        <h1>Furtuna Teo</h1>
-        <h1>Andronie Laurentiu</h1>
-        <h1>Ruxi Martoiu</h1> 
-        <h1>Dumitru Mihnea-Ioan</h1>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
   );
 }
-
-export default App;
---
+++ client/src/components/Header.js
@@ -0,0 +1,8 @@
+import React from "react";
+export default function Header() {
+    return (
+        <div>
+            <h1>Hello</h1>
+        </div>
+    );
+}
