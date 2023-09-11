25e21c437eb2b2c8838ee2ec0f12b08a2d86a0ea
Miu Radu Andrei <miu.andrei@yahoo.com>
2023-09-11 20:52:18 +0300

    add buttons

---
+++ client/src/components/ActivitatiPrincipale.js
@@ -0,0 +1,20 @@
+import React from "react";
+export default function ButuanePrincipale(){
+    return (
+    <div>
+        <Button
+            color="primary"
+            outline
+        >
+            Liga Legando
+        </Button>
+
+        <Button
+            color="primary"
+            outline
+        >
+            Joc cu impuscaturi
+        </Button>
+    </div>
+    );
+}
--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -1,9 +1,12 @@
 import React from "react";
 import Header from "../components/Header";
-
+import ActivitatiPrincipale from "../components/ActivitatiPrincipale";
 
 export default function Home() {
   return (
+    <>
     <Header/>
+    <ButoanePrincipale/>
+    </>
   );
 }
