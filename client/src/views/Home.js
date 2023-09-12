7796de6b34fb57119c3b0002aa70767cde32e5b3
AMihneaa <asmihnea5@gmail.com>
2023-09-13 01:04:08 +0300

    Fixed

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -2,19 +2,16 @@ import React from "react";
 import Header from "../components/Header";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
 export default function Home() {
-  return (<>
- <div className="container-fluid">
-    <div className="row">
-      <div className="col-2 bg-primary vh-100 position-fixed left-0">Componenta Stânga (50px)</div>
-      <div className="col-8"><Header/></div>
-      <div className="col-2 bg-danger vh-100 position-fixed right-0">Componenta Dreapta (50px)</div>
-    </div>
-  </div>
-   
-
+  return (
+    <>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-1 bg-primary vh-100 left-0">Componenta Stânga (10%)</div>
+          <div className="col-10 w-full"><Header /></div>
+          <div className="col-1 bg-danger vh-100 fixed right-0">Componenta Dreapta (10%)</div>
+        </div>
+      </div>
     </>
-   
   );
 }
