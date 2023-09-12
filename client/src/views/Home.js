bc42099bae992dd852cfa738ca2d943d45a8d8c3
TeoStorm7 <teodorfurtuna@yahoo.com>
2023-09-13 00:27:10 +0300

    Test message

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -1,9 +1,13 @@
 import React from "react";
 import Header from "../components/Header";
+import Testde from "../components/Testde";
 
 
 export default function Home() {
   return (
+    <div>
     <Header/>
-  );
+    <Testde/>
+    </div>
+    );
 }
