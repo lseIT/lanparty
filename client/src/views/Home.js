7d37073ea1909ba2ad729577f3eb6d93dbaf75cc
AMihneaa <asmihnea5@gmail.com>
2023-09-13 12:11:55 +0300

    aia e

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -4,12 +4,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import AboutLanparty from "../components/AboutLanparty";
 import AboutUs from "../components/AboutUs";
 
+
 export default function Home() {
   return (
     <>
       <div className="container-fluid">
-        <div className="row">
-          <div className="col-md-1  col-2 bg-black h-100 left-0">
+        <div className="row position-absolute">
+          <div className="col-md-1  col-2 bg-black h-100 left-0" >
             <div className="col-md-6 col-8 bg-danger h-screen mx-auto"></div>
           </div>
           <div className="col-md-10 center col-8 w-full  bg-black">
