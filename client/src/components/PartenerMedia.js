d978683a65b1b42182e4cc588f2bc7c29c0eeff9
smiromania <tedygamer7@gmail.com>
2023-09-14 19:52:27 +0300

    adaugat niste smecherii

--- client/src/components/PartenerMedia.js
+++ client/src/components/PartenerMedia.js
@@ -1,52 +1,52 @@
 import React from "react";
-function DualColorButton( text1, text2 ) {
+function TikTokButton( text1, text2 ) {
     const totalLength = text1.length + text2.length;
     const ratio1 = text1.length / totalLength;
     const ratio2 = 1 - ratio1;
 
     return (
-      <button className="flex border rounded-lg mx-4">
-        <a href="https://www.tiktok.com/@allstarsgamingro">
+      <button className="flex mx-4">
+        <a className="font-bold no-underline" href="https://www.tiktok.com/@allstarsgamingro">
         <span className="rounded-l-lg text-white bg-red-500 px-10 py-2" style={{ flex: ratio1 }}>
           {text1}
         </span>
-        <span className="rounded-r-lg text-white bg-blue-500 px-10 py-2" style={{ flex: ratio2 }}>
+        <span className="rounded-lg text-white bg-blue-500 px-10 py-3" style={{ flex: ratio2 }}>
           {text2}
         </span>
         </a>
       </button>
     );
   }
-  function DualColorButton1( text1, text2 ) {
+  function InstagramButton( text1, text2 ) {
     const totalLength = text1.length + text2.length;
     const ratio1 = text1.length / totalLength;
     const ratio2 = 1 - ratio1;
 
     return (
-      <button className="flex border rounded-lg mx-4">
-        <a href="https://www.instagram.com/allstarsgamingro/">
+      <button className="flex mx-4">
+        <a className="font-bold no-underline" href="https://www.instagram.com/allstarsgamingro/">
         <span className="rounded-l-lg text-white bg-red-500 px-10 py-2" style={{ flex: ratio1 }}>
           {text1}
         </span>
-        <span className="rounded-r-lg text-white bg-blue-500 px-10 py-2" style={{ flex: ratio2 }}>
+        <span className="rounded-lg text-white bg-blue-500 px-10 py-3" style={{ flex: ratio2 }}>
           {text2}
         </span>
         </a>
       </button>
     );
   }
-  function DualColorButton2( text1, text2 ) {
+  function YouTubeButton( text1, text2 ) {
     const totalLength = text1.length + text2.length;
     const ratio1 = text1.length / totalLength;
     const ratio2 = 1 - ratio1;
 
     return (
-      <button className="flex border rounded-lg">
-        <a href="https://www.youtube.com/channel/UCju1YCClDspmThj_iifpeUw">
+      <button className="flex rounded-lg">
+        <a className="font-bold no-underline" href="https://www.youtube.com/channel/UCju1YCClDspmThj_iifpeUw">
         <span className="rounded-l-lg text-white bg-red-500 px-10 py-2" style={{ flex: ratio1 }}>
           {text1}
         </span>
-        <span className="rounded-r-lg text-white bg-blue-500 px-10 py-2" style={{ flex: ratio2 }}>
+        <span className="rounded-lg text-white bg-blue-500 px-10 py-3" style={{ flex: ratio2 }}>
           {text2}
         </span>
         </a>
@@ -63,12 +63,12 @@ function PartenerMedia () {
         <br></br>
         <br></br>
         <br></br>
-        <center><h3 className='pb-32 text-white'>Pentru ediția curentă de LANParty, avem onoarea să colaborăm cu una dintre organizațiile eSports de prestigiu din România: <a className="text-[#78e800] underline" href="">AllStars Gaming</a> ca partener media oficial.</h3></center>
-        <center><a href="INSERT LINK BUTON HERE"><button className="bg-[#78e800] text-black font-bold py-2 px-20 rounded"> BUTONUL NOSTRU    </button></a></center>
+        <center><h3 className='pb-32 text-white'>Pentru ediția curentă de LANParty, avem onoarea să colaborăm cu una dintre organizațiile eSports de prestigiu din România: <a className="text-[#78e800] underline" href="https://allstars-gaming.ro/">AllStars Gaming</a> ca partener media oficial.</h3></center>
+        <center className="pb-16"><a href="INSERT LINK BUTON HERE"><button className="bg-[#78e800] text-black font-bold py-2 px-20 rounded"> BUTONUL NOSTRU    </button></a></center>
             <div className="flex justify-center pt-9">
-                {DualColorButton("50k", "TikTok")}
-                {DualColorButton1("116k", "Instagram")}
-                {DualColorButton2("1.07M", "YouTube")}
+                {TikTokButton("50k", "TikTok")}
+                {InstagramButton("116k", "Instagram")}
+                {YouTubeButton("1.07M", "YouTube")}
             </div>
         </>
     )
