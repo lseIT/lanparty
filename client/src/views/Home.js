1e775176102bd7f8132a693ff1958886f2d7ff20
smiromania <tedygamer7@gmail.com>
2023-09-14 19:36:27 +0300

    Adaugat dupa Header PartenerMedia

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -11,7 +11,8 @@ export default function Home() {
           <div className="col-md-1 col-2 bg-black vh-100 left-0">
             <div className="col-md-6 col-8 bg-danger vh-100 mx-auto"></div>
           </div>
-          <div className="col-md-10 col-8 w-full bg-black"><Header /></div>
+          <div className="col-md-10 col-8 w-full bg-black"><Header />
+          <PartenerMedia /></div>
           <div className="col-md-1 col-2 bg-black vh-100 fixed right-0">
             <div className="col-md-6 col-8 bg-primary vh-100 mx-auto"></div>
           </div>
