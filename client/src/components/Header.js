2dab9769274f57b9c7fc67d357d1b9ed36c92294
dulgheriubogdan <dulgheriubogdan30@gmail.com>
2023-09-14 22:29:44 +0300

    Done the footer

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Header() {
-    return (
-        <div>
-            <h1 className="text-white">Hello</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1 className="text-white">Hello</h1>
+    </div>
+  );
 }
