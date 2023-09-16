8ce94013d6dd618145ddb6ea27bf56c1a67c6fc3
Mihnea2203 <mihneaioan.dumitru@gmail.com>
2023-09-16 17:26:42 +0300

    Carousel

--- client/src/App.js
+++ client/src/App.js
@@ -26,6 +26,38 @@ function App() {
         <h1>Ruxi Martoiu</h1> 
         <h1>Dumitru Mihnea-Ioan</h1>
       </header>
+      <script>
+      const carouselItems = document.querySelectorAll('.carousel-item');
+let currentIndex = 0;
+
+function showSlide(index) {
+  
+  carouselItems.forEach(item => {
+    item.style.display = 'none';
+    })};
+
+  
+  carouselItems[index].style.display = 'block';
+
+
+function nextSlide() {
+  currentIndex = (currentIndex + 1) % carouselItems.length};
+  showSlide(currentIndex);
+
+
+function previousSlide() {
+  currentIndex = (currentIndex - 1 + carouselItems.length) % carouselItems.length};
+  showSlide(currentIndex);
+
+
+
+showSlide(currentIndex);
+
+
+document.getElementById('nextBtn').addEventListener('click', nextSlide);
+document.getElementById('prevBtn').addEventListener('click', previousSlide);
+
+      </script>
     </div>
   );
 }
