3d265b9ab59052314c50b5638210c54eba17861a
octav <octavianvaleanu25@gmail.com>
2023-09-17 11:28:23 +0300

    Am pus background simplu, si am sters pozele luand in considerare ca nu am nevoie de ele

--- client/src/components/Background.js
+++ client/src/components/Background.js
@@ -1,38 +1,22 @@
 import React from "react";
 
 export default function Background() {
-    //array cu imaginile folosite
-    /*
-    const backgroundImages = [
-        'BackgroundPictures/LOLPicture/LOL1.jpg',
-        'BackgroundPictures/LOLPicture/LOL2.jpg',
-        'BackgroundPictures/LOLPicture/LOL3.jpg',
-        'BackgroundPictures/LOLPicture/LOL4.png',
-        'BackgroundPictures/CSGOPicture/CS1.jpg',
-        'BackgroundPictures/CSGOPicture/CS2.jpg',
-        'BackgroundPictures/CSGOPicture/CS3.jpg',
-        'BackgroundPictures/CSGOPicture/CS4.jpg',
-        // Add more image URLs as needed
-    ];
-    */
-
     const containerStyle = {
-        backgroundImage: `url(${require('./BackgroundPictures/Neutral1.jpg')})`,
+        backgroundColor: '#0a5500',
         backgroundSize: 'cover',
         backgroundPosition: 'center',
         width: '100%',
-        height: '100vh',
+        height: `100vh`,
+        //height: `${window.innerHeight.toString()}vh`,
         display: 'flex',
-        //flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
     }
     return (
 
-        //TODO make it change occasionally
         <div style={containerStyle}>
 
-            <h1>Background image brings me pain </h1>
+            <h1>Background image brings me pain in the ass </h1>
         </div>
     );
 };
