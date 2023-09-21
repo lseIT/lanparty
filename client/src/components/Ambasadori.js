72d6661b7f2ee2cd97a958acc2a1b20b728e99be
TeoStorm7 <teodorfurtuna@yahoo.com>
2023-09-21 22:33:55 +0300

    add amb2

--- client/src/components/Ambasadori.js
+++ client/src/components/Ambasadori.js
@@ -1,50 +1,107 @@
 import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import poza1 from "../assets/img/webp/gallery/poze-lan-1.webp";
+import poza2 from "../assets/img/webp/gallery/poze-lan-7.webp";
+import poza3 from "../assets/img/webp/gallery/poze-lan-3.webp";
+import poza4 from "../assets/img/webp/gallery/poze-lan-8.webp";
+import poza5 from "../assets/img/webp/gallery/poze-lan-5.webp";
+import poza6 from "../assets/img/webp/gallery/poze-lan-6.webp";
 
-export default function Ambasadori() {
+const MyCarousel = () => {
   return (
-    <div className="carousel rounded-box">
-      <div className="carousel-item">
-        <img
-          src="https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80"
-          alt="Burger"
-        />
+    <div className=" ">
+      {/* For Mobile Phone */}
+      <div className="md:hidden flex justify-center ">
+        <h1 className="uppercase mt-10 text-2xl md:text-3xl font-bold whitespace-nowrap p-4 mb-6 text-white">
+          Ambasadori <span className=" text-color-accent">Lanparty</span>
+        </h1>
       </div>
-      <div className="carousel-item">
-        <img
-          src="https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80"
-          alt="Burger"
-        />
+      <div className="md:hidden">
+        <Carousel showStatus={false} showThumbs={false} emulateTouch={true}>
+          {/* Slide 1 */}
+          <div className=" flex gap-10 mt-5 justify-center items-center">
+            <div className="w-full">
+              <img src={poza1} alt="Image 1" />
+            </div>
+          </div>
+
+          {/* Slide 2 */}
+          <div className=" flex gap-10 mt-5 justify-center items-center">
+            <div className="w-full">
+              <img src={poza2} alt="Image 1" />
+            </div>
+          </div>
+
+          {/* Slide 3 */}
+          <div className="flex gap-10 mt-5 justify-center items-center">
+            <div className="w-full">
+              <img src={poza3} alt="Image 1" />
+            </div>
+          </div>
+
+          {/* Slide 4 */}
+          <div className="flex gap-10 mt-5 justify-center items-center">
+            <div className="w-full">
+              <img src={poza4} alt="Image 1" />
+            </div>
+          </div>
+
+          {/* Slide 5 */}
+          <div className=" flex gap-10 mt-5 justify-center items-center">
+            <div className="w-full">
+              <img src={poza5} alt="Image 1" />
+            </div>
+          </div>
+
+          {/* Slide 6 */}
+          <div className=" flex gap-10 mt-5 justify-center items-center">
+            <div className="w-full">
+              <img src={poza6} alt="Image 1" />
+            </div>
+          </div>
+        </Carousel>
       </div>
-      <div className="carousel-item">
-        <img
-          src="https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80"
-          alt="Burger"
-        />
-      </div>
-      <div className="carousel-item">
-        <img
-          src="https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80"
-          alt="Burger"
-        />
-      </div>
-      <div className="carousel-item">
-        <img
-          src="https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80"
-          alt="Burger"
-        />
-      </div>
-      <div className="carousel-item">
-        <img
-          src="https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80"
-          alt="Burger"
-        />
-      </div>
-      <div className="carousel-item">
-        <img
-          src="https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80"
-          alt="Burger"
-        />
+
+      {/* For Desktop */}
+      <div className="hidden md:block ">
+        <div className=" flex justify-center ">
+          <h1 className="uppercase mt-10 text-2xl md:text-3xl font-bold whitespace-nowrap p-4 mb-6 text-white">
+            Ambasadori <span className=" text-color-accent">Lanparty</span>
+          </h1>
+        </div>
+        <Carousel showStatus={false} showThumbs={false} emulateTouch={true}>
+          {/* Slide 1 */}
+          <div className=" flex gap-10 mt-5 justify-center items-center">
+            <div className="w-1/3">
+              <img src={poza1} alt="Image 1" />
+            </div>
+            <div className="w-1/3">
+              <img src={poza2} alt="Image 2" />
+            </div>
+            <div className="w-1/3">
+              <img src={poza3} alt="Image 3" />
+            </div>
+          </div>
+
+          {/* Slide 2 */}
+          <div className="flex gap-10 mt-5 justify-center items-center">
+            <div className="w-1/3">
+              <img src={poza4} alt="Image 4" />
+            </div>
+            <div className="w-1/3">
+              <img src={poza5} alt="Image 5" />
+            </div>
+            <div className="w-1/3">
+              <img src={poza6} alt="Image 6" />
+            </div>
+          </div>
+
+          {/* ... Slide 2, Slide 3, Slide 4 ... */}
+        </Carousel>
       </div>
     </div>
   );
-}
+};
+
+export default MyCarousel;
