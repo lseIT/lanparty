573b37f2d66f2bf009b78281eb06ef7937261762
Miu Radu Andrei <miu.andrei@yahoo.com>
2023-10-01 22:23:51 +0300

    update titlu butoanePrincipale

--- client/src/components/ActivitatiPrincipale.js
+++ client/src/components/ActivitatiPrincipale.js
@@ -9,7 +9,7 @@ export default function ButoanePrincipale(){
     return (
     
     <ul id="cards">
-    <div className="mb-20 lg:mb-32  w-full flex justify-center  font-extrabold leading-none tracking-tight text-[#78e800] md:scale-x-100 text-2-5xl lg:text-7xl md:text-5xl sm:text-3xl">Competitii principale</div>
+    <div className="mb-20 lg:mb-32  w-full flex justify-center  font-extrabold leading-none tracking-tight text-[#78e800] text-1234xl lg:text-7xl md:text-5xl sm:text-3xl">Competitii principale</div>
         <li className="lol">
             <a target="_blank" href="https://forms.gle/zvgg1Yw8ScXwKX9UA">
                 <div className="w-full h-full"><img src={lol}/></div>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
         "color-ev": "#060c2e",
         "color-accent": "#78e800",
       },
+      fontSize: {
+        '1234xl': '1.752rem',
+      }
     },
   },
   plugins: [],
